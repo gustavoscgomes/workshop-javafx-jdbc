d3cb60571c5c311d5860b0a1cefaa71a2557829a
Gustavo Gomes <gugaasousa@gmail.com>
2025-09-20 19:19:43 -0300

    Main view controller

--- src/main/java/io/github/gustavoscgomes/workshopjavafxjdbc/HelloController.java
+++ src/main/java/io/github/gustavoscgomes/workshopjavafxjdbc/HelloController.java
@@ -1,14 +1,39 @@
 package io.github.gustavoscgomes.workshopjavafxjdbc;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction(){
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction(){
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction(){
+        System.out.println("onMenuItemAboutAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
