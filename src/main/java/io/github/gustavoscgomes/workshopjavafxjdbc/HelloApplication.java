23d6a2fdbcb7ad92b3dd60ffa57225c113e4a4c4
Gustavo Gomes <gugaasousa@gmail.com>
2025-09-20 19:08:59 -0300

    Main view design

--- src/main/java/io/github/gustavoscgomes/workshopjavafxjdbc/HelloApplication.java
+++ src/main/java/io/github/gustavoscgomes/workshopjavafxjdbc/HelloApplication.java
@@ -2,7 +2,9 @@ package io.github.gustavoscgomes.workshopjavafxjdbc;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +13,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        ScrollPane scrollPane= fxmlLoader.load();
+
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
