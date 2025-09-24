ea94b8d140e2744c20d974cec8f683e88f5ff822
Gustavo Gomes <gugaasousa@gmail.com>
2025-09-24 18:17:55 -0300

    DepartmentFormController

---
+++ src/main/java/io/github/gustavoscgomes/workshopjavafxjdbc/controller/DepartmentFormController.java
@@ -0,0 +1,48 @@
+package io.github.gustavoscgomes.workshopjavafxjdbc.controller;
+
+import io.github.gustavoscgomes.workshopjavafxjdbc.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepartmentFormController implements Initializable {
+
+    @FXML
+    private TextField textFieldId;
+
+    @FXML
+    private TextField textFieldName;
+
+    @FXML
+    private Label labelErrorName;
+
+    @FXML
+    private Button buttonSave;
+
+    @FXML
+    private Button buttonCancel;
+
+    @FXML
+    public void onBtSaveAction() {
+        System.out.println("onBtSaveAction");
+    }
+
+    @FXML
+    public void onBtCancelAction() {
+        System.out.println("onBtCancelAction");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    private void initializeNodes() {
+        Constraints.setTextFieldInteger(textFieldId);
+        Constraints.setTextFieldMaxLength(textFieldName, 30);
+    }
+}
